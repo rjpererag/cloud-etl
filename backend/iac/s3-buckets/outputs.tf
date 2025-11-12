7569ba18152fb1668fa0786e552be5f320a510c8
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2025-11-12 13:07:29 +0100

    feat/adding outputs to be used by ecs-service

--- backend/iac/s3-buckets/outputs.tf
+++ backend/iac/s3-buckets/outputs.tf
@@ -1,9 +1,25 @@
 # Values to display after deployment
-# backend/iac/outputs.tf
+# backend/iac/s3-bucket/outputs.tf
+
+output "raw_bucket_name" {
+  description = "The Name of the S3 raw data bucket for environment variables."
+  value       = aws_s3_bucket.raw_bucket.bucket
+}
 
 output "raw_bucket_id" {
   description = "The ID of the S3 raw data bucket"
   value = aws_s3_bucket.raw_bucket.id
+}
+
+output "processed_bucket_name" {
+  description = "The Name of the S3 processed data bucket for environment variables."
+  value       = aws_s3_bucket.processed_bucket.bucket
+}
+
+output "processed_bucket_arn" {
+  description = "The ARN of the S3 processed data bucket for IAM policies."
+  # This is the crucial ARN required by the ECS Task Role policy
+  value       = aws_s3_bucket.processed_bucket.arn
 }
 
 output "etl_execution_role_arn" {
